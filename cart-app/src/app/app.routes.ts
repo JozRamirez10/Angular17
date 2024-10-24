9aa362c57498b24a4f2d142b3f17f5f55e7ce057
Joz Fama <jm_rg@comunidad.unam.mx>
2024-10-24 10:36:59 -0600

    Separando los proyectos de cart-app en diferentes apps

--- cart-app/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { CartComponent } from './components/cart/cart.component';
-import { CatalogComponent } from './components/catalog/catalog.component';
-
-export const routes: Routes = [
-    {path: '', redirectTo: '/catalog', pathMatch: 'full'}, // Redirige la página de inicio a /catalog
-    {path: 'cart', component: CartComponent},
-    {path: 'catalog', component: CatalogComponent}
-];
--- cart-app/src/app/models/cartItem.ts
+++
@@ -1,6 +0,0 @@
-import { Product } from "./product";
-
-export class CartItem{
-    quantity : number = 0;
-    product ! : Product
-}
